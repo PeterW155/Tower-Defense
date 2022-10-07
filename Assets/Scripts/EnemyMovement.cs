bb549f4ef5763d46e9011ab2d2e9dafd9e4fdac9
SuperChrisBoy <lele2000213@gmail.com>
2022-10-07 19:10:43 -0400

    Initial pathfinding commit

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -13,6 +13,8 @@ public class EnemyMovement : MonoBehaviour
     void Start()
     {
         target = Waypoints.waypoints[0];
+
+        // Call the pathfinding algorithm A*
     }
 
     // Update is called once per frame
@@ -25,6 +27,8 @@ public class EnemyMovement : MonoBehaviour
         {
             GetNextWaypoint();
         }
+
+        // If terrain is changed, call the pathfinding algorithm again
     }
 
     void GetNextWaypoint()
@@ -37,5 +41,32 @@ public class EnemyMovement : MonoBehaviour
 
         wavepointIndex++;
         target = Waypoints.waypoints[wavepointIndex];
+    }
+
+    void AStarPathFinding()
+    {
+        /* 
+        OPEN_LIST
+        CLOSED_LIST
+        ADD start_cell to OPEN_LIST
+
+        LOOP
+            current_cell = cell in OPEN_LIST with the lowest F_COST
+            REMOVE current_cell from OPEN_LIST
+            ADD current_cell to CLOSED_LIST
+
+        IF current_cell is finish_cell
+            RETURN
+
+        FOR EACH adjacent_cell to current_cell
+            IF adjacent_cell is unwalkable OR adjacent_cell is in CLOSED_LIST
+                SKIP to the next adjacent_cell
+
+            IF new_path to adjacent_cell is shorter OR adjacent_cell is not in OPEN_LIST
+                SET F_COST of adjacent_cell
+                SET parent of adjacent_cell to current_cell
+                IF adjacent_cell is not in OPEN_LIST
+                    ADD adjacent_cell to OPEN_LIST
+        */
     }
 }
