c6dd9cf03a04bbab30ecd047430537a26fb5ff74
seanpgillick <75447516+seanpgillick@users.noreply.github.com>
2022-10-19 00:51:46 -0400

    Tutorial 13 - Lives/Enemy Values
    Added Lives and the ability to damage enemies. Enemies now provide 50 gold when killed.

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -7,8 +7,13 @@ public class PlayerStats : MonoBehaviour
     public static int Money;
     public int startMoney = 1000;
 
+    public static int Lives;
+    public int startLives = 20;
+
     void Start ()
     {
         Money = startMoney;
+        Lives = startLives;
     }
+
 }
