9633f7f87b9e7927ebc6f8941e0acc2170e4dd7d
SuperChrisBoy <lele2000213@gmail.com>
2022-10-25 21:43:53 -0400

    Group selection and navmesh pathfinding

---
+++ Assets/Scripts/Unit/UnitClick.cs
@@ -0,0 +1,63 @@
+
+using UnityEngine;
+
+public class UnitClick : MonoBehaviour
+{
+    private Camera myCam;
+    public GameObject groundMarker;
+
+    public LayerMask clickableLayer;
+    public LayerMask ground;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit = new RaycastHit();
+            Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, clickableLayer))
+            {
+                // If we hit a clickable object
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    // Shift clicked
+                    UnitSelections.Instance.ShiftClickSelect(hit.collider.gameObject);
+                }
+                else
+                {
+                    // Normal Clicked
+                    UnitSelections.Instance.ClickSelect(hit.collider.gameObject);
+                }
+            }
+            else
+            {
+                // If we didn't and we're not shift clicking
+                if (!Input.GetKey(KeyCode.LeftShift))
+                {
+                    UnitSelections.Instance.DeselectAll();
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ground))
+            {
+                groundMarker.transform.position = hit.point;
+                groundMarker.SetActive(false);
+                groundMarker.SetActive(true);
+            }
+        }
+    }
+}
