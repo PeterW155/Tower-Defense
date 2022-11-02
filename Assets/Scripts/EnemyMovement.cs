f5b46bf69d45742ef99c94978bd65cb4a074baed
Unlockingice <jpo39@drexel.edu>
2022-11-02 10:27:31 -0400

    Added/Modified Enemy Scripts

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,20 +5,12 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
 
-    public float speed = 10f;
-    private Transform target;
-    private int wavepointIndex = 0;
-
     public int value = 75;
 
     public GameObject deathEffect;
 
     public int health = 100;
-    // Start is called before the first frame update
-    void Start()
-    {
-        target = Waypoints.waypoints[0];
-    }
+
 
     public void TakeDamage (int amount)
     {
@@ -39,33 +31,5 @@ public class EnemyMovement : MonoBehaviour
         Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 dir = target.position - transform.position;
-        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
 
-        if (Vector3.Distance(transform.position, target.position) <= .2f)
-        {
-            GetNextWaypoint();
-        }
-    }
-
-    void GetNextWaypoint()
-    {
-        if (wavepointIndex >= Waypoints.waypoints.Length-1)
-        {
-            EndPath();
-            return;
-        }
-
-        wavepointIndex++;
-        target = Waypoints.waypoints[wavepointIndex];
-    }
-
-    void EndPath()
-    {
-        PlayerStats.Instance.lives--;
-        Destroy(gameObject);
-    }
 }
