01f4518584ea40159ecd80d3400d2c56afdd30c2
sg3527 <sg3527@cs.drexel.edu>
2022-11-30 17:12:44 -0500

    UI Changes, Troop Spawning, Enemy Health Bars

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyMovement : MonoBehaviour
 {
@@ -9,12 +10,22 @@ public class EnemyMovement : MonoBehaviour
 
     public GameObject deathEffect;
 
-    public int health = 100;
+    public int startHealth = 100;
+    private float health;
 
+    [Header("Unity Stuff")]
+    public Image healthBar;
+
+
+    void Start ()
+    {
+        health = startHealth;
+    }
 
     public void TakeDamage (int amount)
     {
         health -= amount;
+        healthBar.fillAmount = health/startHealth;
         if (health <= 0)
         {
             Die();
