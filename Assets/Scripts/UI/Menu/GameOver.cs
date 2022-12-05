7a38fdb18388bb920c807df834b6223d4823fa55
sg3527 <75447516+seanpgillick@users.noreply.github.com>
2022-12-05 15:09:51 -0500

    Pause Menu - Merge Conflicts

--- Assets/Scripts/UI/Menu/GameOver.cs
+++ Assets/Scripts/UI/Menu/GameOver.cs
@@ -11,7 +11,8 @@ public class GameOver : MonoBehaviour
 
     void OnEnable()
     {
-        roundsText.text = PlayerStats.Instance.rounds.ToString();
+        if (roundsText != null)
+            roundsText.text = PlayerStats.Instance.rounds.ToString();
     }
 
     public void Retry ()
@@ -22,6 +23,36 @@ public class GameOver : MonoBehaviour
     public void Menu ()
     {
         SceneLoader.Instance.Load("MainMenu", true);
+    }
+
+    public void Continue()
+    {
+        GameObject UI = GameObject.Find("GamePausedUI");
+        UI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        GameObject UI = FindInActiveObjectByName("GamePausedUI");
+        UI.SetActive(true);
+    }
+
+    GameObject FindInActiveObjectByName(string name)
+    {
+        Transform[] objs = Resources.FindObjectsOfTypeAll<Transform>() as Transform[];
+        for (int i = 0; i < objs.Length; i++)
+        {
+            if (objs[i].hideFlags == HideFlags.None)
+            {
+                if (objs[i].name == name)
+                {
+                    return objs[i].gameObject;
+                }
+            }
+        }
+        return null;
     }
 }
 
