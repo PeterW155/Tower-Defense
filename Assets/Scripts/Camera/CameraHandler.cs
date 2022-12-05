3007756ea7d9c90d7562375b0380f58a6eae4651
maraudical <gradymilligan@icloud.com>
2022-12-05 18:27:20 -0500

    Camera updates
    added league of legends mouse on edge of screen to move control. Also made it so that camera zooming will not clip through the ground collision. Camera can also automatically constrain itself to the world border preventing it from moving outside the world

--- Assets/Scripts/Camera/CameraHandler.cs
+++ Assets/Scripts/Camera/CameraHandler.cs
@@ -20,6 +20,12 @@ public class CameraHandler : MonoBehaviour
     public bool lockLookX = true;
     [Tooltip("Only necessary if lock look X is false.")]
     public Vector2 minMaxLookX;
+    [Space]
+    public bool screenEdgeMoving = true;
+    public float screenEdgeRatio = 0.05f;
+    [Space]
+    public bool lockToWorldBorder = true;
+    [Space]
     [Range(1, 5)]
     public float movementSensetivity = 2.5f;
     [Range(1, 50)]
@@ -33,6 +39,8 @@ public class CameraHandler : MonoBehaviour
     [Range(1, 50)]
     public float zoomDrag = 10f;
     public Vector2 zoomMinMax = new Vector2(2, 20);
+    [Space]
+    public LayerMask clippingCollision;
 
     [HideInInspector]
     public PlayerInput playerInput;
@@ -80,12 +88,16 @@ public class CameraHandler : MonoBehaviour
 
     float defaultDrag = 0.95f;
 
+    bool IsMouseOverGameWindow { get { return !(0 > Mouse.current.position.ReadValue().x || 0 > Mouse.current.position.ReadValue().y || Screen.width < Mouse.current.position.ReadValue().x || Screen.height < Mouse.current.position.ReadValue().y); } }
+
     //might be better to separate the player input component from the camera handler object in the future
     private static CameraHandler _instance;
     public static CameraHandler Instance { get { return _instance; } }
 
     void Awake()
     {
+        Physics.queriesHitBackfaces = true;
+
         // If an instance of this already exists and it isn't this one
         if (_instance != null && _instance != this)
         {
@@ -192,23 +204,65 @@ public class CameraHandler : MonoBehaviour
     {
         if (!lockControls)
         {
+            //screen edge moving happens first so that it is overwritten if move keys are used
+            Vector2 screenPosition = Mouse.current.position.ReadValue();
+            move = Vector2.zero;
+
+            if (screenEdgeMoving && Application.isFocused && IsMouseOverGameWindow)
+            {
+                if (screenPosition.y / Screen.height < screenEdgeRatio) //bottom
+                    move = new Vector2(move.x, -1);
+                else if (screenPosition.y / Screen.height > 1 - screenEdgeRatio) //top
+                    move = new Vector2(move.x, 1);
+                if (screenPosition.x / Screen.width < screenEdgeRatio)  //left
+                    move = new Vector2(-1, move.y);
+                else if (screenPosition.x / Screen.width > 1 - screenEdgeRatio) //right
+                    move = new Vector2(1, move.y);
+            }
+
             if (_rotate.WasPressedThisFrame() || _rotateAlt.WasPressedThisFrame() || _zoomAlt.WasPressedThisFrame())
-                warpPosition = Mouse.current.position.ReadValue();
-            move = _move.ReadValue<Vector2>();
+                warpPosition = screenPosition;
+
+            Vector2 keyMove = _move.ReadValue<Vector2>();
+            move = keyMove.magnitude > 0 || move.magnitude == 0 ? keyMove : move; //overwrite if move keys pressed
             look = _look.ReadValue<Vector2>();
             lookAlt = _lookAlt.ReadValue<float>();
             zoom = _zoom.ReadValue<Vector2>().normalized.y;
+
+            //check if moveing past world border
+            if (lockToWorldBorder && World.Instance != null)
+            {
+                Vector2 borderMinMax = new Vector2(World.Instance.worldData.border - 0.5f, World.Instance.worldData.chunkSize * World.Instance.worldData.mapSizeInChunks - World.Instance.worldData.border - 0.5f);
+                if (transform.position.x < borderMinMax.x)
+                {
+                    transform.position = new Vector3(borderMinMax.x, transform.position.y, transform.position.z);
+                    _rigidbodyParent.velocity = new Vector3(Mathf.Abs(_rigidbodyParent.velocity.x) * 0.7f, _rigidbodyParent.velocity.y, _rigidbodyParent.velocity.z);
+                }
+                else if (transform.position.x > borderMinMax.y)
+                {
+                    transform.position = new Vector3(borderMinMax.y, transform.position.y, transform.position.z);
+                    _rigidbodyParent.velocity = new Vector3(-Mathf.Abs(_rigidbodyParent.velocity.x) * 0.7f, _rigidbodyParent.velocity.y, _rigidbodyParent.velocity.z);
+                }
+                if (transform.position.z < borderMinMax.x)
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y, borderMinMax.x);
+                    _rigidbodyParent.velocity = new Vector3(_rigidbodyParent.velocity.x, _rigidbodyParent.velocity.y, Mathf.Abs(_rigidbodyParent.velocity.z) * 0.7f);
+                }
+                else if (transform.position.z > borderMinMax.y)
+                {
+                    transform.position = new Vector3(transform.position.x, transform.position.y, borderMinMax.y);
+                    _rigidbodyParent.velocity = new Vector3(_rigidbodyParent.velocity.x, _rigidbodyParent.velocity.y, -Mathf.Abs(_rigidbodyParent.velocity.z) * 0.7f);
+                }
+            }
 
             //look X (optional)
             if (!lockLookX && (_rotateAlt.IsPressed() || _rotate.IsPressed()))
             {
                 cameraXRotate.localEulerAngles += new Vector3(10f * -look.y * rotationSensetivity * Time.unscaledDeltaTime, 0, 0);
 
-                Debug.Log(cameraXRotate.localEulerAngles.x);
                 //check if camera is near x limits
                 if (cameraXRotate.localEulerAngles.x > minMaxLookX.y && cameraXRotate.localEulerAngles.x < minMaxLookX.x)
                 {
-                    Debug.Log("too high");
                     float target = Mathf.Abs(minMaxLookX.y - cameraXRotate.localEulerAngles.x) > Mathf.Abs(minMaxLookX.x - cameraXRotate.localEulerAngles.x) ? minMaxLookX.x : minMaxLookX.y;
                     cameraXRotate.localEulerAngles = new Vector3(target, 0, 0);
                 }
@@ -223,28 +277,48 @@ public class CameraHandler : MonoBehaviour
                 zoomTime = 0;
                 zoomPosZ = cameraZoom.localPosition.z;
                 Vector2 lookMinMax = new Vector2(look.x > look.y ? look.y : look.x, look.x > look.y ? look.x : look.y);
-                zoomTarget = Mathf.Clamp(zoomTarget + Mathf.Clamp(look.x + look.y, lookMinMax.x, lookMinMax.y) * zoomSensetivity * 2f * Time.unscaledDeltaTime, -zoomMinMax.y, -zoomMinMax.x);
+                zoomTarget = Mathf.Clamp(zoomTarget + Mathf.Clamp(look.x + look.y, lookMinMax.x, lookMinMax.y) * -zoomSensetivity * 2f * Time.unscaledDeltaTime, zoomMinMax.x, zoomMinMax.y);
             }
             //camera zoom
             else if (!_zoomAlt.IsPressed() && zoom != 0)
             {
                 zoomTime = 0;
                 zoomPosZ = cameraZoom.localPosition.z;
-                //float zoomAmount = Time.deltaTime * 40f * zoomSensetivity; //this is for scroll wheel
-                //zoomTarget = Mathf.Clamp(zoomTarget + (look.y > 0 ? zoomAmount : -zoomAmount), -zoomMinMax.y, -zoomMinMax.x);
                 float valueAdjustment = playerInput.currentControlScheme == "Controller" ? 10f : 120f;
-                zoomTarget = Mathf.Clamp(zoomTarget + zoom * zoomSensetivity * valueAdjustment * Time.unscaledDeltaTime, -zoomMinMax.y, -zoomMinMax.x);
+                zoomTarget = Mathf.Clamp(zoomTarget + zoom * -zoomSensetivity * valueAdjustment * Time.unscaledDeltaTime, zoomMinMax.x, zoomMinMax.y);
             }
         }
-        
+
+        //check for zoom world clipping
+        Vector3 furthestPoint = GetFurthestPoint(transform.position, cameraXRotate.forward, zoomMinMax.y) + cameraXRotate.forward; //the float value is the offset from the ground
+        float dist = Vector3.Distance(furthestPoint, transform.position);
+
+        Vector3 GetFurthestPoint(Vector3 start, Vector3 dir, float distance)
+        {
+            if (Physics.Raycast(start, dir, out RaycastHit hit, distance, clippingCollision))
+            {
+                Vector3 point = hit.point + (dir.normalized * 0.01f);
+                return GetFurthestPoint(point, dir, distance - Vector3.Distance(start, point));
+            }
+            else
+                return start;
+        }
+
+        if (dist > Mathf.Abs(zoomTarget))
+        {
+            zoomTarget = dist;
+        }
+
         if (zoomTime < (5 / zoomDrag))
         {
-            zoomTime += Time.deltaTime;
+            zoomTime += Time.unscaledDeltaTime;
         }
         else
         {
             zoomTime = (5 / zoomDrag);
         }
+
+        Debug.DrawLine(transform.position, furthestPoint, Color.red);
 
         cameraZoom.localPosition = new Vector3(0, 0, Mathf.Lerp(zoomPosZ, zoomTarget, Mathf.SmoothStep(0f, 1f, Mathf.Pow(zoomTime / (5 / zoomDrag), 0.4f))));
     }
@@ -252,7 +326,7 @@ public class CameraHandler : MonoBehaviour
     private void FixedUpdate()
     {
         //camera movement
-        Vector2 p_move = move.normalized;
+        Vector2 p_move = -move.normalized; //inverted because of the direction of the zoom parent
         Vector3 rotateOffset = cameraYRotate.TransformDirection(new Vector3(p_move.x, 0, p_move.y));
         p_move = new Vector2(rotateOffset.x, rotateOffset.z).normalized;
 
